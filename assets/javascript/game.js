b1665c641851785127d62060fe5841f423267be7
Alissa Yamazaki <alissa.yamazaki@gmail.com>
2019-08-30 21:45:35 -0400

    added jQuery on click function but not fully working

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+ // Create on-click event that responds to button clicks of the crystal image.
+ $(document).ready(function() {
+
+ $(".crystal-image").on("click", function() {
+
+    // Clicking the button triggers an alert message.
+    alert("You clicked a crystal!");
+
+  });
+
+});
