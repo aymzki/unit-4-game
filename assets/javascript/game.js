8b2463922dc11cf4265a9deb6bb57bb16a15e49c
Alissa Yamazaki <alissa.yamazaki@gmail.com>
2019-08-31 07:14:23 -0400

    created more options for points

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,40 @@
- //Document ready makes sure webpage is ready before starting jQuery code
- $(document).ready(() => {
+//Document ready makes sure webpage is ready before starting jQuery code
+$(document).ready(() => {
 
- //Set "targetNumber" that the user should be aiming to reach
- var targetNumber = 50;
+    //Set "targetNumber" that the user should be aiming to reach
+    var targetNumber = 50;
 
- //Set "number-to-guess" header to match targetNumber
- $("#number-to-guess").text(targetNumber);
+    //Set "number-to-guess" header to match targetNumber
+    $("#number-to-guess").text(targetNumber);
 
 
- //Made a counter to track user's total
- var counter = 0;
+    //Made a counter to track user's total
+    var counter = 0;
+
+    //Select a number from this array as points, randomize
+    var numberOptions = [10, 11]
+    var increment = numberOptions[Math.round(Math.random())];
+
     // Create on-click event that responds to button clicks of the crystal image.   
- $(".crystal-image").on("click", function() {
+    $(".crystal-image").on("click", function () {
 
-    //Increment by 1 every time user clicks the crystal
-    counter += 10;
+        //Increment by that random number every time user clicks the crystal
+        counter += increment;
 
-    // Clicking the button triggers an alert message.
-    alert("Your new score is: " + counter);
+        // Clicking the button triggers an alert message.
+        alert("Your new score is: " + counter);
 
-    //Check if click counter matches targetNumber
-    if (counter === targetNumber) {
+        //Check if click counter matches targetNumber
+        if (counter === targetNumber) {
 
         //If numbers match, celebrate the win
         alert("You win!");
-    }
+        }
 
-    //If the user's counter exceeds targetNumber they lose.
-    else if (counter >= targetNumber) {
+        //If the user's counter exceeds targetNumber they lose.
+        else if (counter >= targetNumber) {
 
         alert("You lose!");
     }
-
-  });
 
 });
