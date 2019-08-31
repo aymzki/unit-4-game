972def40b0766c7ec9cd6543620c4ae1d941f93d
Alissa Yamazaki <alissa.yamazaki@gmail.com>
2019-08-30 22:46:05 -0400

    added points

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,16 @@
- // Create on-click event that responds to button clicks of the crystal image.
+ //Document ready makes sure webpage is ready before starting jQuery code
  $(document).ready(() => {
 
+ //Made a counter to track user's total
+ var counter = 0;
+    // Create on-click event that responds to button clicks of the crystal image.   
  $(".crystal-image").on("click", function() {
 
+    //Increment by 1 every time user clicks the crystal
+    counter += 10;
+
     // Clicking the button triggers an alert message.
-    alert("You clicked a crystal!");
+    alert("Your new score is: " + counter);
 
   });
 
