62bc44108308f9e62bd9d7542f2926970d9138a6
Alissa Yamazaki <alissa.yamazaki@gmail.com>
2019-09-01 21:48:55 -0400

    assigned random points to all four crystals and made it reset every time basically finished the javascript backbone

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,34 +7,82 @@ $(document).ready(() => {
     //Set "number-to-guess" header to match targetNumber
     $("#number-to-guess").text(targetNumber);
 
-
     //Made a counter to track user's total
     var counter = 0;
+    var youWin = 0;
+    var youLose = 0;
+    
+    generateNumber()
+    
+    function generateNumber() {
 
-    //Select a number from this array as points, randomize
-    var numberOptions = [10, 11]
-    var increment = numberOptions[Math.round(Math.random())];
+        for (var i = 0; i < 5; i++) {
+
+            var index = i + 1
+
+            var randomNumber = Math.floor(Math.random() * 20);
+
+            //each iteration will have a var imageCrystal
+            var imageCrystal = $("#crystal" + index);
+
+            //each imageCrystal will be assigned a data attribute depending on number options
+            imageCrystal.attr("data-crystalvalue", randomNumber);
+
+        }
+
+    }
+
 
     // Create on-click event that responds to button clicks of the crystal image.   
     $(".crystal-image").on("click", function () {
 
+        var crystalValue = ($(this).attr("data-crystalvalue"));
+        crystalValue = parseInt(crystalValue);
+
         //Increment by that random number every time user clicks the crystal
-        counter += increment;
+        counter += crystalValue;
+        console.log(crystalValue);
+
+        $("#totalScore").text("Your total score is: " + counter);
+
 
         // Clicking the button triggers an alert message.
+
         alert("Your new score is: " + counter);
+
 
         //Check if click counter matches targetNumber
         if (counter === targetNumber) {
 
-        //If numbers match, celebrate the win
-        alert("You win!");
+            youWin++;
+            $("#wins").append(youWin);
+
+            //If numbers match, celebrate the win
+            alert("You win!");
+
+            counter = 0;
+
+            generateNumber();
+
         }
 
         //If the user's counter exceeds targetNumber they lose.
         else if (counter >= targetNumber) {
 
-        alert("You lose!");
-    }
+            youLose++;
+            $("#losses").append(youLose);
+
+            alert("You lose!");
+
+            counter = 0;
+
+            generateNumber();
+        }
+
+
+
+    });
+
+
 
 });
